f7e5ea03cb20cb62d83c9e61f971dd50a35a5071
Kevin Benoit <79103494+KevinBenoitt@users.noreply.github.com>
2024-01-05 14:09:02 +0100

    update

--- sample-app.sh
+++ sample-app.sh
@@ -6,10 +6,19 @@ if [ ! -d "tempdir" ]; then
     mkdir tempdir/templates
     mkdir tempdir/static
 
-    cp sample_app.py tempdir/.
-    cp -r templates/* tempdir/templates/.
-    cp -r static/* tempdir/static/.
+    # Clone de repo als deze niet bestaat, anders voer een pull uit
+    if [ ! -d "cicd-sample-app" ]; then
+        git clone https://github.com/KevinBenoitt/cicd-sample-app.git
+    else
+        git -C cicd-sample-app pull origin main
+    fi
 
+    # Kopieer bestanden naar tempdir
+    cp cicd-sample-app/sample_app.py tempdir/.
+    cp -r cicd-sample-app/templates/* tempdir/templates/.
+    cp -r cicd-sample-app/static/* tempdir/static/.
+
+    # Docker-opbouw
     cat > tempdir/Dockerfile << _EOF_
     FROM python
     RUN pip install flask
@@ -21,7 +30,10 @@ if [ ! -d "tempdir" ]; then
 _EOF_
 fi
 
+# Navigeer naar tempdir
 cd tempdir || exit
+
+# Bouw de Docker-image en draai de container
 docker build -t sampleapp .
 docker run -t -d -p 5050:5050 --name samplerunning sampleapp
 docker ps -a
